94615a77625358bb8974357492af6971012d5761
Felix Kahmann <felix@kahmann.com>
2017-01-07 22:30:41 +0100

    mess with jQuery getJSON v4

--- js/objecthandling.js
+++ js/objecthandling.js
@@ -2,16 +2,7 @@ function loadItems(){
   var finished = false;
   $.getJSON("json/items.json", function (data){
     items =data;
-  })
-
-  .done(function (){
     finished = true;
-  })
-
-  .fail(function(){
-    finished = true;
-    alert("Failed to retireve item-database from the server!");
-    throw "Faile to retrieve \"json/items.json\"";
   });
 
   var operationvar = 0;
